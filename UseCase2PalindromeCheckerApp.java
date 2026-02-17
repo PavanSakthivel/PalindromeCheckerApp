bfbfda0d7061068e55ab1ce72adbb75646fd1af1
PavanSakthivel <sakpavan@gmail.com>
2026-02-17 10:36:00 +0530

    Updated UC2 logic

--- UseCase2PalindromeCheckerApp.java
+++ UseCase2PalindromeCheckerApp.java
@@ -3,22 +3,15 @@ import java.util.Scanner;
 class UseCase2PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input: ");
+        System.out.print("Input text: ");
         String input = sc.nextLine();
-        boolean pal = isPalindrome(input);
-        if (pal){
-            System.out.println("Is it a palindrome? :" + " true");
-        } else {
-            System.out.println("Is it a palindrome? :" + " false");
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
         }
-        sc.close();
-        
-        
-    }
-    
-    public static boolean isPalindrome(String str) {
-        String cleanedStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        String reversedStr = new StringBuilder(cleanedStr).reverse().toString();
-        return cleanedStr.equals(reversedStr);
+        System.out.println("Is it a palindrome? : " + isPalindrome);
     }
 }
