9163eb7549d824693da726cf0ffad3f834f258c4
PavanSakthivel <sakpavan@gmail.com>
2026-02-17 10:50:08 +0530

    UC3 logic created

---
+++ UseCase3PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class UseCase3PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String input = sc.nextLine();
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+        boolean isPalindrome = input.equals(reversed);
+        System.out.println("Is it a palindrome? : " + isPalindrome);
+    }
+}
+
