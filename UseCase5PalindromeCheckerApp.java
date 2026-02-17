a35a0230b1a91dacb549425e99fafb2cc9f5f0df
PavanSakthivel <sakpavan@gmail.com>
2026-02-17 11:10:40 +0530

    UC5 logic created

---
+++ UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class UseCase5PalindromeCheckerApp {
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Input: ");
+    String input = sc.nextLine();    
+    Stack<Character> stack = new Stack<>();
+
+    for (char c : input.toCharArray()) {
+         stack.push(c);
+    }
+
+    boolean isPalindrome = true;
+
+    for (char c : input.toCharArray()) {
+        if (c != stack.pop()) {
+            isPalindrome = false;
+             break;
+        }
+    }
+    System.out.println("Is it a palindrome? : " + isPalindrome);
+    sc.close();
+}
+}
