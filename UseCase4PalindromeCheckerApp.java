5ed59b8d7c8666fb8aa95c68efa3ff5ba6af15ff
PavanSakthivel <sakpavan@gmail.com>
2026-02-17 10:59:32 +0530

    UC4 created

---
+++ UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class UseCase4PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String input = sc.nextLine();
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println("Is it a palindrome? : " + isPalindrome);
+        sc.close();
+    }
+}
