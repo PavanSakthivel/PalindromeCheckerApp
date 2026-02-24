beae27e8748291eb444c88d401654f48977596b4
PavanSakthivel <sakpavan@gmail.com>
2026-02-24 09:07:36 +0530

    UC11 added

---
+++ UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+
+        PalindromeService service = new PalindromeService();
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Is Palindrome? : " + result);
+        sc.close();
+    }
+}
+
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
